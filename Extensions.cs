462c72fe36762e764260b6f7da6f82fe16f62c0a
espaus <espen.austefjord@hotmail.com>
2017-11-16 20:35:29 +0100

    added shuffle functionality through extension class and method

---
+++ Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqExample
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>
+            (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqExample
 {
     class Program
     {
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            
+            foreach (var c in startingDeck)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            var top = startingDeck.Take(26);
+            var bottom = startingDeck.Skip(26);
+            var shuffle = top.InterleaveSequenceWith(bottom);
+
+            foreach (var c in shuffle)
+            {
+                Console.WriteLine(c);
+            }
         }
     }
 }
